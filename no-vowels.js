103cf8be802aec24191ec9eacc39e5503d06f477
Sean Tracey <seanmtracey@gmail.com>
2016-05-12 16:56:41 +0100

    Added no-vowels + .gitignore

---
+++ no-vowels.js
@@ -0,0 +1,14 @@
+var articleParas = document.querySelectorAll('#storyContent p');
+
+for(var x = 0; x < articleParas.length; x += 1){
+	
+	var p = articleParas[x];
+		
+	var childNodes = p.childNodes;
+	
+	for(var y = 0; y < childNodes.length; y += 1){
+		var child = childNodes[y];
+		child.textContent = child.textContent.replace(/[aeiou]/gi, '');
+	}
+	
+}
